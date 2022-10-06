6bd3f19b8ff8521a70ac27b17b0e993c599724da
middleware <malifarge.thomas9@gmail.com>
2022-10-06 16:13:21 +0200

    correction setup

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,21 @@
+import { useAllPrismicDocumentsByTag } from "@prismicio/react"
+
+import H1 from "../Components/H1"
+import H2 from "../Components/H2"
 
 const Home = () =>{
+
+    const [documents] = useAllPrismicDocumentsByTag('Séries')
+
+    console.log(documents);
+
     return(
         <> 
-        <h1>Hello</h1>    
+            <H1>Home</H1>
+            <H2>Séries</H2>
+            <section>
+
+            </section>
         </>
     )
 }
--- src/prismic.js
+++ src/prismic.js
@@ -3,11 +3,11 @@ import * as prismic from '@prismicio/client'
 export const repositoryName = 'Mon-blog'
 
 export const client = prismic.createClient(repositoryName, {
-  accessToken: '',
+  accessToken: 'MC5ZejdiMHhFQUFDUUFpT0RW.KX_vv70o77-9fX_vv704Zu-_vSxQW--_ve-_vVkFWu-_vUzvv70PU1JEUe-_vSXvv73vv70B',
   
   routes: [
     {
-      type: 'homepage',
+      type: 'post',
       path: '/',
     },
   ],
